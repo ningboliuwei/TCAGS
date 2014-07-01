58ae1e18c21e0df750c53cb3359793f343a3f56b
ningboliuwei <liuwei@nbu.edu.cn>
2014-07-01 16:09:31 +0800

    自动生成代码文件以及default.build文件功能。

--- TCAGS/TestProgramGenerator/Form1.cs
+++ TCAGS/TestProgramGenerator/Form1.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace TestProgramGenerator
 {
@@ -19,8 +21,63 @@ namespace TestProgramGenerator
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			GenerateCSFile(textBox1.Text, "r:\\source.cs");
+			string outputDir = Application.StartupPath;
+			string className = GetClassName(textBox1.Text);
 
+			GenerateCSFile(textBox1.Text, outputDir + "\\" + className + ".cs");
+			GenerateBuildConfig(className, outputDir);
+
+			MessageBox.Show("输出成功");
+		}
+
+		private string GetClassName(string sourceCode)
+		{
+			string codeText = "";
+			int startPos;
+			int endPos;
+			string className;
+
+			codeText = sourceCode.Replace(" ", "").Replace("\r", "").Replace("\n", "").ToLower();
+			startPos = codeText.IndexOf("class") + 5;
+			endPos = codeText.IndexOf("{", startPos);
+			className = codeText.Substring(startPos, endPos - startPos - 1);
+
+			return className;
+		}
+
+		private void GenerateBuildConfig(string className, string outputDir)
+		{
+			string originalConfigText;
+			string newConfigText;
+			string templatePath;
+			string targetPath;
+
+
+			templatePath = outputDir + "\\default_build_template.xml";
+			targetPath = outputDir + "\\default.build";
+
+			XmlDocument doc = new XmlDocument();
+
+			try
+			{
+				doc.Load(templatePath);
+				originalConfigText = doc.OuterXml;
+
+				newConfigText = originalConfigText.Replace("$build_dir", outputDir)
+					.Replace("$source_dll_name", className + ".dll")
+					.Replace("$source_cs_name", className + ".cs")
+					.Replace("$test_dll_name", className + "test.dll")
+					.Replace("$test_cs_name", className + "test.cs")
+					.Replace("$report_output_dir", outputDir);
+
+				doc.LoadXml(newConfigText);
+				doc.Save(targetPath);
+			}
+			catch (Exception ex)
+			{
+
+				throw new Exception(ex.Message);
+			}
 
 		}
 
