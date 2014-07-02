33efc2d28bea6fa43d5a2ceb1620af6f7d8dbc94
ningboliuwei <liuwei@nbu.edu.cn>
2014-07-02 14:41:10 +0800

    基本完成自动执行nunit并输出测试结果功能。

--- TCAGS/TestProgramGenerator/Form1.cs
+++ TCAGS/TestProgramGenerator/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -21,11 +22,21 @@ namespace TestProgramGenerator
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			string outputDir = Application.StartupPath;
+			string outputDir = "r:\\";
 			string className = GetClassName(textBox1.Text);
+			string command;
 
 			GenerateCSFile(textBox1.Text, outputDir + "\\" + className + ".cs");
 			GenerateBuildConfig(className, outputDir);
+
+			command = outputDir.Substring(0, 2) + "\n";
+			command += "cd " + outputDir + "\n";
+			command += "c:\\\n";
+			//command += "cd c:\\windows\\system32\n";
+			command += "NANT.BAT";
+
+			ExecuteCommand(command);
+
 
 			MessageBox.Show("输出成功");
 		}
@@ -53,7 +64,7 @@ namespace TestProgramGenerator
 			string targetPath;
 
 
-			templatePath = outputDir + "\\default_build_template.xml";
+			templatePath = Application.StartupPath + "\\default_build_template.xml";
 			targetPath = outputDir + "\\default.build";
 
 			XmlDocument doc = new XmlDocument();
@@ -68,7 +79,8 @@ namespace TestProgramGenerator
 					.Replace("$source_cs_name", className + ".cs")
 					.Replace("$test_dll_name", className + "test.dll")
 					.Replace("$test_cs_name", className + "test.cs")
-					.Replace("$report_output_dir", outputDir);
+					.Replace("$report_output_dir", outputDir)
+					.Replace("$nunit_dll_path", Application.StartupPath + "\\nunit.framework.dll");
 
 				doc.LoadXml(newConfigText);
 				doc.Save(targetPath);
@@ -96,5 +108,31 @@ namespace TestProgramGenerator
 			}
 
 		}
+
+
+		private void ExecuteCommand(string command)
+		{
+			Process p = new Process();
+			p.StartInfo.FileName = "cmd.exe";
+
+			string[] commands = command.Split('\n');
+
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.CreateNoWindow = true;
+
+			p.Start();
+
+			foreach (string cmd in commands)
+			{
+				p.StandardInput.WriteLine(cmd);
+			}
+			p.StandardInput.WriteLine("exit");
+			p.WaitForExit();
+
+			p.Close();
+		}
+
 	}
 }
